624c701b5d7b5aabc42109e2d2f4f8e250f7bb15
Trotyl Yu <yzj1995@vip.qq.com>
2015-02-27 19:35:34 +0800

    Configure the Portal route

--- Portal/urls.py
+++ Portal/urls.py
@@ -5,8 +5,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Zinc.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    # url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('Portal.urls')),
+    url(r'^$', 'Portal.views.home', name='home'),
 
 )
