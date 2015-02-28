81bfc8dc87d26179ac60118bbf0d74c99af675ae
Trotyl Yu <yzj1995@vip.qq.com>
2015-02-28 16:12:26 +0800

    Add helper zipfile

---
+++ Portal/helpers/zip_dir.py
@@ -0,0 +1,39 @@
+import os
+import os.path
+import zipfile
+
+
+def zip_dir(dir_name, zipfilename):
+    file_list = []
+    if os.path.isfile(dir_name):
+        file_list.append(dir_name)
+    else:
+        for root, dirs, files in os.walk(dir_name):
+            for name in files:
+                file_list.append(os.path.join(root, name))
+         
+    zf = zipfile.ZipFile(zipfilename, "w", zipfile.zlib.DEFLATED)
+    for tar in file_list:
+        arc_name = tar[len(dir_name):]
+        zf.write(tar, arc_name)
+    zf.close()
+ 
+ 
+def unzip_file(zipfilename, unziptodir):
+    if not os.path.exists(unziptodir):
+        os.mkdir(unziptodir, 0o777)
+    zfobj = zipfile.ZipFile(zipfilename)
+    for name in zfobj.namelist():
+        name = name.replace('\\', '/')
+        
+        if name.endswith('/'):
+            os.mkdir(os.path.join(unziptodir, name))
+        else:            
+            ext_filename = os.path.join(unziptodir, name)
+            ext_dir = os.path.dirname(ext_filename)
+            if not os.path.exists(ext_dir):
+                os.mkdir(ext_dir,0o777)
+            outfile = open(ext_filename, 'wb')
+            outfile.write(zfobj.read(name))
+            outfile.close()
+
