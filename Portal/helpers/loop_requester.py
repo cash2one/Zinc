eaa2967fbaa4bd60bd0c2d385776b0fe4739518b
Trotyl Yu <yzj1995@vip.qq.com>
2015-02-28 20:19:36 +0800

    Add test for BaiduFilter and amented

--- Portal/helpers/loop_requester.py
+++ Portal/helpers/loop_requester.py
@@ -1,7 +1,7 @@
 import urllib
 import re
 
-from urllib import request, parse
+from urllib import request
 from urllib.error import HTTPError
 from collections import deque
 
@@ -9,99 +9,12 @@ from collections import deque
 # Create your models here.
 
 
-def mkdir(path):
-    import os
-    path = path.strip()
-    path = path.rstrip('\\')
-    exists = os.path.exists(path)
-
-    if not exists:
-        os.makedirs(path)
-        return True
-    else:
-        return False
-
-
-def get_host(url):
-    match = re.match('http[s]?://([^/"\']+)', url)
-    if match:
-        return match.group(0)
-    return ''
-
-
-def get_domain(url):
-    host = get_host(url)
-    idx = host.find('://')
-    return host[idx + 3:]
-
-
-def get_parent(url):
-    url = url.rstrip('/')
-    idx = url.rfind('/')
-    return url[:idx]
-
-
-def get_full_url(url, current=''):
-    if url.startswith('http'):
-        return url
-    if url == '/':
-        return get_host(current)
-    if url.startswith('#'):
-        return get_parent(current)
-    if url.startswith('//'):
-        return 'http:' + url
-    if url.startswith('/'):
-        return get_host(current) + url
-    else:
-        return get_parent(current) + '/' + url
-
-
-def get_data(url):
-    try:
-        res = request.urlopen(url)
-        data = res.read()
-        full_url = res.geturl()
-    except HTTPError:
-        data = False
-        full_url = ''
-    return data, full_url
-
-
-def get_list(data):
-    try:
-        data = data.decode('UTF-8')
-        url_list = re.findall('(?:href|src)=\"([^\'\"]+)\"', data)
-        return list(set(url_list))
-    except UnicodeDecodeError:
-        return []
-
-
-def save_data(data, url):
-    if not url.startswith('http'):
-        return
-    if url.startswith('#'):
-        return
-    if url.endswith('/'):
-        url += 'index.html'
-    elif url == get_host(url):
-        url += '/index.html'
-    elif url.rfind('.') < url.rfind('/'):
-        url += '.html'
-    idxl = url.find('://')
-    idxr = url.rfind('/')
-    path = url[idxl + 3: idxr]
-    mkdir('download/' + path)
-    file = open('download/' + url[idxl + 3:], 'wb')
-    file.write(data)
-    file.close()
-
-
 class LoopSpider:
 
     def __init__(self, start_page):
         if not start_page.startswith('http'):
             start_page = 'http://' + start_page
-        host = get_host(start_page)
+        host = self.get_host(start_page)
         if host == '':
             return
         self.host = host
@@ -114,20 +27,98 @@ class LoopSpider:
             return set()
         while len(self.queue) != 0 and len(self.record) < 20:
             current = self.queue.popleft()
-            data, url = get_data(current)
+            data, url = self.get_data(current)
             if not data or url in self.record:
                 continue
             self.record.add(url)
-            save_data(data, url)
-            url_list = get_list(data)
+            self.save_data(data, url)
+            url_list = self.get_list(data)
             self.append_queue(url_list, current)
         return self.record
 
     def append_queue(self, url_list, current):
         for link in url_list:
-            url = get_full_url(link, current)
-            if not get_host(url) == self.host:
+            url = self.get_full_url(link, current)
+            if not self.get_host(url) == self.host:
                 continue
             if not url in self.record and not url in self.queue:
                 self.queue.append(url)
 
+    def mkdir(self, path):
+        import os
+        path = path.strip()
+        path = path.rstrip('\\')
+        exists = os.path.exists(path)
+
+        if not exists:
+            os.makedirs(path)
+            return True
+        else:
+            return False
+
+    def get_host(self, url):
+        match = re.match('http[s]?://([^/"\']+)', url)
+        if match:
+            return match.group(0)
+        return ''
+
+    def get_domain(self, url):
+        host = self.get_host(url)
+        idx = host.find('://')
+        return host[idx + 3:]
+
+    def get_parent(self, url):
+        url = url.rstrip('/')
+        idx = url.rfind('/')
+        return url[:idx]
+
+    def get_full_url(self, url, current=''):
+        if url.startswith('http'):
+            return url
+        if url == '/':
+            return self.get_host(current)
+        if url.startswith('#'):
+            return self.get_parent(current)
+        if url.startswith('//'):
+            return 'http:' + url
+        if url.startswith('/'):
+            return self.get_host(current) + url
+        else:
+            return self.get_parent(current) + '/' + url
+
+    def get_data(self, url):
+        try:
+            res = request.urlopen(url)
+            data = res.read()
+            full_url = res.geturl()
+        except HTTPError:
+            data = False
+            full_url = ''
+        return data, full_url
+
+    def get_list(self, data):
+        try:
+            data = data.decode('UTF-8')
+            url_list = re.findall('(?:href|src)=\"([^\'\"]+)\"', data)
+            return list(set(url_list))
+        except UnicodeDecodeError:
+            return []
+
+    def save_data(self, data, url):
+        if not url.startswith('http'):
+            return
+        if url.startswith('#'):
+            return
+        if url.endswith('/'):
+            url += 'index.html'
+        elif url == self.get_host(url):
+            url += '/index.html'
+        elif url.rfind('.') < url.rfind('/'):
+            url += '.html'
+        idxl = url.find('://')
+        idxr = url.rfind('/')
+        path = url[idxl + 3: idxr]
+        self.mkdir('download/' + path)
+        file = open('download/' + url[idxl + 3:], 'wb')
+        file.write(data)
+        file.close()
--- Portal/tests.py
+++ Portal/tests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
-from Portal.helpers.loop_requester import LoopSpider, get_host, get_domain, get_full_url, get_parent, get_data, get_list
+from Portal.helpers.loop_requester import LoopSpider
+from Portal.helpers.topic_filter import BaiduFilter
 
 
 class LoopSpiderTestCase(TestCase):
     def setUp(self):
-        pass
+        self.spider = LoopSpider('')
 
     def test_get_host_of_url(self):
         url1 = 'http://www.baidu.com'
@@ -13,19 +14,19 @@ class LoopSpiderTestCase(TestCase):
         url4 = 'https://www.baidu.com/'
         url5 = 'http://www.baidu.com/index'
         url6 = 'http://www.baidu.com/index.html'
-        self.assertEqual(get_host(url1), url1)
-        self.assertEqual(get_host(url2), url1)
-        self.assertEqual(get_host(url3), url3)
-        self.assertEqual(get_host(url4), url3)
-        self.assertEqual(get_host(url5), url1)
-        self.assertEqual(get_host(url6), url1)
+        self.assertEqual(self.spider.get_host(url1), url1)
+        self.assertEqual(self.spider.get_host(url2), url1)
+        self.assertEqual(self.spider.get_host(url3), url3)
+        self.assertEqual(self.spider.get_host(url4), url3)
+        self.assertEqual(self.spider.get_host(url5), url1)
+        self.assertEqual(self.spider.get_host(url6), url1)
 
     def test_get_domain_of_url(self):
         url0 = 'www.baidu.com'
         url1 = 'http://www.baidu.com'
         url2 = 'https://www.baidu.com'
-        self.assertEqual(get_domain(url1), url0)
-        self.assertEqual(get_domain(url2), url0)
+        self.assertEqual(self.spider.get_domain(url1), url0)
+        self.assertEqual(self.spider.get_domain(url2), url0)
 
     def test_get_parent_of_url(self):
         url1 = 'http://www.baidu.com'
@@ -35,12 +36,12 @@ class LoopSpiderTestCase(TestCase):
         url5 = 'http://www.baidu.com/index.html/'
         url6 = 'http://www.baidu.com/index/index.html'
         url7 = 'http://www.baidu.com/index/index.html/'
-        self.assertEqual(get_parent(url2), url1)
-        self.assertEqual(get_parent(url3), url1)
-        self.assertEqual(get_parent(url4), url1)
-        self.assertEqual(get_parent(url5), url1)
-        self.assertEqual(get_parent(url6), url2)
-        self.assertEqual(get_parent(url7), url2)
+        self.assertEqual(self.spider.get_parent(url2), url1)
+        self.assertEqual(self.spider.get_parent(url3), url1)
+        self.assertEqual(self.spider.get_parent(url4), url1)
+        self.assertEqual(self.spider.get_parent(url5), url1)
+        self.assertEqual(self.spider.get_parent(url6), url2)
+        self.assertEqual(self.spider.get_parent(url7), url2)
 
     def test_full_url_of_url(self):
         url1 = 'http://www.baidu.com'
@@ -49,37 +50,47 @@ class LoopSpiderTestCase(TestCase):
         url4 = 'http://www.baidu.com/index/index.html'
         rel1 = 'test.html'
         rel2 = '/test.html'
-        self.assertEqual(get_full_url(url1), url1)
-        self.assertEqual(get_full_url(url2), url2)
-        self.assertEqual(get_full_url(rel1, url3), 'http://www.baidu.com/test.html')
-        self.assertEqual(get_full_url(rel2, url3), 'http://www.baidu.com/test.html')
-        self.assertEqual(get_full_url(rel1, url4), 'http://www.baidu.com/index/test.html')
-        self.assertEqual(get_full_url(rel2, url4), 'http://www.baidu.com/test.html')
+        self.assertEqual(self.spider.get_full_url(url1), url1)
+        self.assertEqual(self.spider.get_full_url(url2), url2)
+        self.assertEqual(self.spider.get_full_url(rel1, url3), 'http://www.baidu.com/test.html')
+        self.assertEqual(self.spider.get_full_url(rel2, url3), 'http://www.baidu.com/test.html')
+        self.assertEqual(self.spider.get_full_url(rel1, url4), 'http://www.baidu.com/index/test.html')
+        self.assertEqual(self.spider.get_full_url(rel2, url4), 'http://www.baidu.com/test.html')
 
     def test_get_data_of_url_success(self):
         url = 'https://raw.githubusercontent.com/trotyl/Static/master/fake_data'
-        data, sth = get_data(url)
+        data, sth = self.spider.get_data(url)
         data = data.decode('UTF-8')
         self.assertEqual(data, 'ABCD\n')
 
     def test_get_data_of_url_fail(self):
         url = 'https://raw.githubusercontent.com/trotyl/Static/master/not_exist'
-        data, sth = get_data(url)
+        data, sth = self.spider.get_data(url)
         self.assertFalse(data)
 
     def test_get_list_of_url_many(self):
         url = 'https://raw.githubusercontent.com/trotyl/Static/master/fake_baidu.html'
-        data, sth = get_data(url)
-        url_list = get_list(data)
+        data, sth = self.spider.get_data(url)
+        url_list = self.spider.get_list(data)
         self.assertEqual(len(url_list), 126)
 
     def test_get_list_of_url_zero(self):
         url = 'https://raw.githubusercontent.com/trotyl/Static/master/empty.html'
-        data, sth = get_data(url)
-        url_list = get_list(data)
+        data, sth = self.spider.get_data(url)
+        url_list = self.spider.get_list(data)
         self.assertEqual(url_list, [])
 
     def test_loop_state(self):
         spider = LoopSpider('https://raw.githubusercontent.com/trotyl/Static/master/index.html')
         record = spider.start()
         self.assertEqual(len(record), 5)
+
+
+class BaiduFilterTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_filter_state(self):
+        baidu = BaiduFilter('http://tieba.baidu.com/p/3506527161')
+        topic = baidu.start()
+        self.assertGreaterEqual(len(topic), 53)
